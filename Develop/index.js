23f6416405080afb06ac8b70f3567168be93bdb5
mohanad.saqr <mohanad.saqr@alum.utoronto.ca>
2024-03-20 18:31:21 -0400

    deleted:    Develop/README.md

--- Develop/index.js
+++ Develop/index.js
@@ -1,54 +1,54 @@
-const fs = require("fs");
-const inquirer = require("inquirer");
-const generateMarkdown = require("./utils/generateMarkdown");
+const fs = require('fs');
+const inquirer = require('inquirer');
+const generateMarkdown = require('./utils/generateMarkdown');
 
 // Array of questions for user input
 const questions = [
   {
-    type: "input",
-    name: "title",
-    message: "What is the title of your project?",
+    type: 'input',
+    name: 'title',
+    message: 'What is the title of your project?',
   },
   {
-    type: "input",
-    name: "description",
-    message: "Provide a description of your project:",
+    type: 'input',
+    name: 'description',
+    message: 'Provide a description of your project:',
   },
   {
-    type: "input",
-    name: "installation",
-    message: "What are the installation instructions?",
+    type: 'input',
+    name: 'installation',
+    message: 'What are the installation instructions?',
   },
   {
-    type: "input",
-    name: "usage",
-    message: "How do you use this application?",
+    type: 'input',
+    name: 'usage',
+    message: 'How do you use this application?',
   },
   {
-    type: "list",
-    name: "license",
-    message: "Choose a license for your application:",
-    choices: ["MIT", "Apache_2.0", "GPL_3.0", "BSD_3", "None"],
+    type: 'list',
+    name: 'license',
+    message: 'Choose a license for your application:',
+    choices: ['MIT', 'Apache_2.0', 'GPL_3.0', 'BSD_3', 'None'],
   },
   {
-    type: "input",
-    name: "contributing",
-    message: "What are the contribution guidelines?",
+    type: 'input',
+    name: 'contributing',
+    message: 'What are the contribution guidelines?',
   },
   {
-    type: "input",
-    name: "tests",
-    message: "What are the test instructions?",
+    type: 'input',
+    name: 'tests',
+    message: 'What are the test instructions?',
   },
   {
-    type: "input",
-    name: "github",
-    message: "Enter your GitHub Username:",
+    type: 'input',
+    name: 'github',
+    message: 'Enter your GitHub Username:',
   },
   {
-    type: "input",
-    name: "email",
-    message: "Enter your email address:",
+    type: 'input',
+    name: 'email',
+    message: 'Enter your email address:',
   },
 ];
 
@@ -56,7 +56,7 @@ const questions = [
 function writeToFile(fileName, data) {
   fs.writeFile(fileName, data, (err) => {
     if (err) throw err;
-    console.log("The README has been generated!");
+    console.log('The README has been generated!');
   });
 }
 
@@ -64,7 +64,7 @@ function writeToFile(fileName, data) {
 function init() {
   inquirer.prompt(questions).then((answers) => {
     const markdown = generateMarkdown(answers);
-    writeToFile("README.md", markdown);
+    writeToFile('README.md', markdown);
   });
 }
 
--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,18 +1,63 @@
-// TODO: Create a function that returns a license badge based on which license is passed in
-// If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+// Function to return a license badge based on which license is passed in
+function renderLicenseBadge(license) {
+  if (!license || license === 'None') return '';
+  return `![License](https://img.shields.io/badge/license-${encodeURIComponent(license)}-blue.svg)`;
+}
 
-// TODO: Create a function that returns the license link
-// If there is no license, return an empty string
-function renderLicenseLink(license) {}
+// Function to return the license link
+function renderLicenseLink(license) {
+  if (!license || license === 'None') return '';
+  return `https://opensource.org/licenses/${license}`;
+}
 
-// TODO: Create a function that returns the license section of README
-// If there is no license, return an empty string
-function renderLicenseSection(license) {}
+// Function to return the license section of README
+function renderLicenseSection(license) {
+  if (!license || license === 'None') return '';
+  return `## License\nThis project is licensed under the ${license} license. For more information, please visit the [license page](${renderLicenseLink(license)}).`;
+}
 
-// TODO: Create a function to generate markdown for README
+// Function to generate markdown for README
 function generateMarkdown(data) {
   return `# ${data.title}
+
+${renderLicenseBadge(data.license)}
+
+## Description
+
+${data.description}
+
+## Table of Contents
+
+- [Installation](#installation)
+- [Usage](#usage)
+- [License](#license)
+- [Contributing](#contributing)
+- [Tests](#tests)
+- [Questions](#questions)
+
+## Installation
+
+${data.installation}
+
+## Usage
+
+${data.usage}
+
+${renderLicenseSection(data.license)}
+
+## Contributing
+
+${data.contributing}
+
+## Tests
+
+${data.tests}
+
+## Questions
+
+For any questions, please contact me at [${data.email}](mailto:${data.email}).
+
+GitHub: [${data.github}](https://github.com/${data.github})
 
 `;
 }
